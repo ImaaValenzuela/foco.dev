1b883654cb10d12e8e02b66b675bef8265a2ad6f
Imanol Valenzuela <ivalenzuelaeguez607@alumno.unlam.edu.ar>
2026-08-17 10:47:34 -0300

    chore(workspace): use canonical repositories and develop

--- config/repos.mjs
+++ config/repos.mjs
@@ -2,26 +2,26 @@ export const repositories = [
   {
     name: "foco.backend",
     url: "git@github.com:ImaaValenzuela/foco.backend.git",
-    branch: "main"
+    branch: "develop"
   },
   {
     name: "foco.frontend",
     url: "git@github.com:ImaaValenzuela/foco.frontend.git",
-    branch: "main"
+    branch: "develop"
   },
   {
     name: "foco.landing",
-    url: "git@github.com:ImaaValenzuela/mente.landing.git",
-    branch: "main"
+    url: "git@github.com:ImaaValenzuela/foco.landing.git",
+    branch: "develop"
   },
   {
     name: "foco.app",
     url: "git@github.com:ImaaValenzuela/foco.app.git",
-    branch: "main"
+    branch: "develop"
   },
   {
     name: "foco.admin",
-    url: "git@github.com:ImaaValenzuela/mente.admin.git",
-    branch: "main"
+    url: "git@github.com:ImaaValenzuela/foco.admin.git",
+    branch: "develop"
   }
 ];
